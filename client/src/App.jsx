a4b410d7bd086731c7f4946d62010121a05b84ca
saquibA <mohammedsaquibansari82@gmail.com>
2026-03-09 17:05:14 +0530

    Initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+     <h1 className="text-4xl font-bold text-blue-500">
+      Tailwind is Working 🚀
+    </h1>
+  )
+}
+
+export default App;
